b6b41ed3e3d42cafee03ff07a8f6ecbdcf2c1c5c
Marko Nußbaum <marko@marnuba.com>
2023-07-22 17:11:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# gif2code
+
+This is a tool to convert a gif image into data that can easily be used in common 8-bit assemblers. The main target platform is the Foenix family of computers, but it might be useful for other vinatage and retro systems as well.
+
+These systems use a 256 color palette from the full 24 bit color range. Incidentially, the gif format also supports 256 colors and is available in most common graphic programs.
+
+Other tools are available to convert images as well (e.g. FoenixIDE). This tool is mainly aimed to be used in an automatic build process that does not need user interaction (i.e. from a script or batch file), so development cycles in the design of a program might be shortend (save gif to project directory, run build script, run program with updated graphics - all in one command).
+
+## Usage
+
+gif2code <INPUT.GIF>
+
+This produces an INPUT.asm file that can be included by the given assembler.
+Supported assembers are:
+
+gif2code --bin <INPUT.GIF>
+
+This produces two files: INPUT.lut and INPUT.bin. These files can be used from an assembler by including them as binary files.
+
+The format of the files is as follows:
+
+### LUT File
+
+The first byte represents the number of entries.
+
+The entries follow at offest 2. Each entry consists of 4 bytes, just as the LUT in the Foenix computers:
+The first three bytes represent the color value in the order B, G, R.
+The fourth byte is reserved for later usage.
+
+### IMG File
+
+Each byte represents one pixel. The value is the index of the color in the lookup table. The order is line by line from top to bottom, each line from left to right.
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,159 @@
+use std::{fs::File, process::exit, path::PathBuf};
+use gif::DecodeOptions;
+use clap::Parser;
+use std::path::Path;
+use std::io::Write;
+
+#[derive(Parser, Debug)]
+struct Args {
+
+    infile: String,
+
+    //#[arg(short, long)]
+    //outfile: Option<String>,
+
+    //#[arg(short, long)]
+    //palette_file: Option<String>,
+
+    //#[arg(short, long)]
+    //assembler: Option<String>,
+
+    #[arg(short, long)]
+    bin: bool,
+
+    #[arg(short, long)]
+    asm: bool,
+}
+
+fn main() {
+
+    let mut args = Args::parse();
+
+    check_filename(&args.infile);
+
+    // if no args are given, default to asm output
+    if !args.asm && !args.bin {
+        args.asm = true;
+    }
+
+    let input = File::open(&args.infile)
+        .expect("Could not open input file.");
+        
+    let mut asmpath = PathBuf::from(&args.infile);
+    asmpath.set_extension("asm");
+    
+    let mut clutpath = asmpath.clone();
+    clutpath.set_extension("clut");
+    
+    let mut imgpath = clutpath.clone();
+    imgpath.set_extension("img");
+
+    let labelpath = imgpath.clone();
+    let label = String::from(labelpath.file_stem().unwrap().to_string_lossy());
+
+
+    // open files for writing
+    let mut asmfile = if args.asm {
+        Some(File::create(asmpath)
+        .expect("Could not open asm file to write."))
+    } else {
+        None
+    };
+
+    let mut clutfile = if args.bin {
+        Some(File::create(clutpath)
+            .expect("Could not open asm file to write."))
+    } else {
+        None
+    };
+
+    let mut imgfile = if args.bin {
+        Some(File::create(imgpath)
+        .expect("Could not open asm file to write."))
+    } else {
+        None
+    };
+
+    
+    let mut opts = DecodeOptions::new();
+
+    opts.set_color_output(gif::ColorOutput::Indexed);
+    let mut decoder = opts.read_info(input).unwrap();
+
+    if let Some(file) = &mut asmfile {
+        write!(file, "\n{}_clut:\n", label)
+        .expect("error writing to asm file");
+    }
+
+    // write palette
+    if let Some(pal) = decoder.global_palette() {
+        println!("sprite_lut:");
+
+        for i in (0..pal.len()).step_by(3) {
+
+            if let Some(file) = &mut asmfile {
+                write!(file, ".byte ${:02X}, ${:02X}, ${:02X}, $00\n", 
+                    pal[i+2],
+                    pal[i+1],
+                    pal[i],
+                ).expect("error writing to asm file");
+            }
+
+            if let Some(file) = &mut clutfile {
+                file.write_all(&[pal[i+2], pal[i+1], pal[i], 0x00])
+                .expect("error writing to lut file");
+            }
+        }
+    }
+
+    if let Some(file) = &mut asmfile {
+        write!(file, "\n{}_img:\n", label)
+        .expect("error writing to asm file");
+    }
+    
+    // write image data
+    while let Some(frame) = decoder.read_next_frame().unwrap() {
+        for h in 0..frame.height {
+            if let Some(file) = &mut asmfile {
+                write!(file, ".byte").expect("error writing to asm file");
+                for w in 0..frame.width-1 {
+                    let ix = h*frame.width + w;
+                    write!(file, " ${:02X},", frame.buffer[ix as usize])
+                        .expect("error writing to asm file");
+                }
+                write!(file, " ${:02X}\n", frame.buffer[((h+1)*frame.width-1) as usize])
+                    .expect("error writing to asm file");
+            }
+            
+            
+        }
+
+        if let Some(file) = &mut imgfile {
+            file.write_all(&frame.buffer).expect("error writing to img file");
+        }
+        
+    }
+}
+
+fn check_filename(filename: &String) {
+    // check: input must be gif
+    let filepath = Path::new(filename);
+
+    if !filepath.is_file() {
+        println!("Input needs to be a file.");
+        exit(-1);
+    }
+
+    if let Some(ext) = filepath.extension() {
+
+        let exts = ext.to_string_lossy();
+        if exts.to_lowercase() != "gif" {
+            println!("input file needs to have the gif extension");
+            exit(-1);
+        }
+    } else {
+        println!("Input file does not have a file extension");
+        exit(-1);
+    }
+}
+
